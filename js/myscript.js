26c6e6a1c34a3e907591d4ba1afa0646fc5730b2
Emanuele <em.zaccaria@gmail.com>
2020-03-27 17:10:56 +0100

    HTLM e JS modifiche minore e prezzo biglietto

--- js/myscript.js
+++ js/myscript.js
@@ -1,50 +1,42 @@
 // Recupero informazioni
-// recupero l'elemento inpunt dove andrà il nome
+// recupero l'elemento inpunt dove andrà il nome e del valore al suo interno
 var nome = document.getElementById("inome");
-// recupero l'elemento inpunt dove andranno i chilometri
+var ValueNome = nome.value;
+// recupero l'elemento inpunt dove andranno i chilometri e del valore al suo interno
 var km = document.getElementById("ikm");
-// recupero l'elemento inpunt dove andrà il tipo di sconto (in base all'eta)
+var ValueKm = km.value;
+// recupero l'elemento inpunt dove andrà il tipo di sconto (in base all'eta) e del valore al suo interno
 var eta = document.getElementById("isconto");
-
+var ValueEta = eta.value;
 // bottone recupero l'informazione dell'oggetto bottone
 var GeneraButton = document.getElementById('generabutton');
-
+// fai accadere qualcosa al click del dell'oggetto bottone
 GeneraButton.addEventListener('click',
-
+// fai accadere qualcosa:
   function(){
-    // dell'oggetto elemento input recupera il valore al suo interno
-    var ValueNome = nome.value;
-    var ValueKm = km.value;
-    var ValueEta = eta.value;
+    // stampa il valore
     console.log(ValueNome);
     console.log(ValueKm);
     console.log(ValueEta);
   }
-
 );
-
-
-
-
-
-
-// var prezzobase;
-// prezzobase = km * 0.21;
-// // creare lo sconto se possibile
-// var scontoGiovane;
-// scontoGiovane = (prezzobase * 20)/100
-// var scontoAnziani;
-// scontoAnziani = (prezzobase * 40)/100
-//
-// // fornire/calcolare ed applicare lo sconto se possibile
-// var prezzoGiovane;
-// var prezzoAnziani;
-// if (eta < 18) {
-//   prezzoGiovane = prezzobase - scontoGiovane;
-//   document.getElementById('biglietto').innerHTML = prezzoGiovane + " Euro";
-// } else if (eta > 65) {
-//   prezzoAnziani = prezzobase - scontoAnziani;
-//   document.getElementById('biglietto').innerHTML = prezzoAnziani + " Euro";
-// } else {
-//   document.getElementById('biglietto').innerHTML = prezzobase + " Euro";
-// }
+// calcolo del prezzo del biglietto
+var prezzobase;
+prezzobase = km * 0.21;
+// creare lo sconto se possibile
+var scontoGiovane;
+scontoGiovane = (prezzobase * 20)/100
+var scontoAnziani;
+scontoAnziani = (prezzobase * 40)/100
+// fornire/calcolare ed applicare lo sconto se possibile
+var prezzoGiovane;
+var prezzoAnziani;
+if (eta < 18) {
+  prezzoGiovane = prezzobase - scontoGiovane;
+  // STAMPA COSA FARE
+} else if (eta > 65) {
+  prezzoAnziani = prezzobase - scontoAnziani;
+  // STAMPA COSA FARE
+} else {
+  // STAMPA COSA FARE
+}
