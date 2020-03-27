6be2726321f5e8820ce8b9e1fcbaf6e5c3ee1f9b
Emanuele <em.zaccaria@gmail.com>
2020-03-27 15:05:41 +0100

    Starting point

---
+++ js/myscript.js
@@ -0,0 +1,8 @@
+// ricreare dalle screenshot
+// calcolo del biglietto
+// DESCRIZIONE:
+// ll programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+// Il prezzo del biglietto è definito in base ai km (0.21 € al km),
+// ma va applicato uno sconto del 20% per i minorenni
+// e del 40% per gli over 65.
