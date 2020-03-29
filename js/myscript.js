978acf8411053ebf698c202034d5af0225ebeb1a
Emanuele <em.zaccaria@gmail.com>
2020-03-29 14:27:42 +0200

    JS calcolo biglietto funzionante

--- js/myscript.js
+++ js/myscript.js
@@ -26,17 +26,23 @@ GeneraButton.addEventListener('click',
     var scontoGiovane = (prezzobase * 20)/100
     var scontoAnziani = (prezzobase * 40)/100
     // fornire/calcolare ed applicare lo sconto se possibile
-    if (ValueEta < 18) {
+    if (ValueEta === "Under") {
       var prezzoGiovane = prezzobase - scontoGiovane;
       // STAMPA prezzo giovani
+      document.getElementById('ocostobiglietto').innerHTML = prezzoGiovane;
       // stampa tipo di biglietto giovane
-    } else if (eta > 65) {
+      document.getElementById('otipobiglietto').innerHTML = "Biglietto Under 18";
+    } else if (ValueEta === "Over") {
        var prezzoAnziani = prezzobase - scontoAnziani;
-      // STAMPA prezzo anziani
-      // stampa tipo di biglietto anziani
+       // STAMPA prezzo anziani
+       document.getElementById('ocostobiglietto').innerHTML = prezzoAnziani;
+       // stampa tipo di biglietto anziani
+       document.getElementById('otipobiglietto').innerHTML = "Biglietto Over 65";
     } else {
       // stampa prezzo Adulto
+      document.getElementById('ocostobiglietto').innerHTML = prezzobase;
       // stampa tipo biglietto adulto
+      document.getElementById('otipobiglietto').innerHTML = "Biglietto Standard";
     }
   }
 )
